6f3fc05fb1f8f12f2b2c8f47250ba6e7844df880
juliflorezg <forezjuli08@hotmail.com>
2022-11-23 16:53:13 -0500

    feat: pdf-reader, add PDFReader component

---
+++ react/components/PDFReader.tsx
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react'
+
+type Props = {
+  pdfUrl: string
+  width: number
+  height: number
+}
+
+const PDFReader:React.FC<Props> = ({pdfUrl, width, height}) => {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(()=> {
+    setMounted(true)
+  }, [])
+
+  console.log({pdfUrl, width, height})
+  return (
+    mounted
+    ?
+      <div className='flex justify-center'>
+        <object
+          data = {pdfUrl}
+          type = "application/pdf"
+          width = {width}
+          height = {height}
+        >
+          <iframe src={pdfUrl} title="PDF" width={width} height={height}>
+            <p>Este navegador no soporta PDF :c</p>
+          </iframe>
+        </object>
+        </div>
+    :
+      <p>Este es mi lector de PDF</p>
+  )
+}
+
+export default PDFReader
